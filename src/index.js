8123f98e5257eccd1e32b4e556cec25478cf9c7e
Mayza Ferreira <mayza_ferr@hotmail.com>
2023-03-20 15:41:50 -0300

    Added project files

---
+++ src/index.js
@@ -0,0 +1,70 @@
+let apiKey = `7784a4cd4aa2e0c25ead7bd96d585b8a`;
+let apiEndPoint = `https://api.openweathermap.org/data/2.5/weather?`;
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let now = new Date();
+let todayDate = document.querySelector(`#date-today`);
+todayDate.innerHTML = `${
+  days[now.getDay()]
+} / ${now.getHours()}:${now.getMinutes()}`;
+
+function searchLocation(event) {
+  event.preventDefault();
+  let newCity = document.querySelector(`#form1`);
+  let units = "metric";
+  let apiUrl = `${apiEndPoint}q=${newCity.value}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeather);
+  console.log(apiUrl);
+}
+
+function displayWeather(response) {
+  let currentCity = document.querySelector(`#current-city`);
+  currentCity.innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let temp = document.querySelector(`#current-temp`);
+  temp.innerHTML = `${temperature}° `;
+}
+
+function retrievePosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let urlApi = `${apiEndPoint}lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(urlApi).then(displayWeather);
+}
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(retrievePosition);
+}
+
+let btnSearch = document.querySelector(`#btn-search`);
+btnSearch.addEventListener(`click`, searchLocation);
+
+let btnGeolocation = document.querySelector(`#btn-geo`);
+btnGeolocation.addEventListener(`click`, getLocation);
+
+/*function changeToC(event) {
+  event.preventDefault();
+  let newTemp = document.querySelector(`#current-temp`);
+  newTemp.innerHTML = `26° `;
+}
+function changeToF(event) {
+  event.preventDefault();
+  let newTemp = document.querySelector(`#current-temp`);
+  newTemp.innerHTML = `79° `;
+}
+
+let fahren = document.querySelector(`#fahrenheit`);
+fahren.addEventListener(`click`, changeToF);
+
+let celsius = document.querySelector(`#celsius`);
+celsius.addEventListener(`click`, changeToC);
+*/
